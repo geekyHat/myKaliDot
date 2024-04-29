c02a4d597c69351d348ec736f0691c4e0e8b784e
geekyHat <153096081+geekyHat@users.noreply.github.com>
2024-04-29 14:08:02 +0100

    Update install.sh
    update script without verifications

--- install.sh
+++ install.sh
@@ -2,23 +2,11 @@
 #
 #
 
-if ! command -v tmux &> /dev/null
-then
-    echo "install tmux"
-    exit 1
-fi
-
-if ! command -v kitty &> /dev/null
-then
-    echo "install kitty terminal"
-    exit 1
-fi
-
 echo "installing tmp plugin manager for tmux"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 echo "coping tmux conf"
-mkdir -p ~/.config/tmux/ && cp tmux.conf ~/.config/
+mkdir -p ~/.config/tmux/ && cp tmux.conf ~/.config/tmux/
 
 echo "coping kitty conf"
 mkdir -p ~/.config/kitty/ && cp kitty.conf ~/.config/
