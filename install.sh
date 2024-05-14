0bac50da35a2b5c01d326d5de99cb4f29245716d
geekyHat <153096081+geekyHat@users.noreply.github.com>
2024-05-14 01:58:25 +0100

    Update install.sh
    hack fonts

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo "coping tmux conf"
 mkdir -p ~/.config/tmux/ && cp tmux.conf ~/.config/tmux/
 
+# download hack fonts from https://sourcefoundry.org/hack/
 echo "coping kitty conf"
 mkdir -p ~/.config/kitty/ && cp kitty.conf ~/.config/
 
