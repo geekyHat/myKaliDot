fb7b37dbe1a1fd80bc12478ee2dfabb0c737d01d
geekyHat <153096081+geekyHat@users.noreply.github.com>
2025-10-22 21:22:02 +0200

    Update install.sh
    added npm, zoxide and zshrc version.

--- install.sh
+++ install.sh
@@ -2,12 +2,17 @@
 #
 
 # install nvim dependency
-sudo apt install imagemagick -y && sudo apt install libmagickwand-dev -y && sudo apt install liblua5.1-0-dev -y && sudo apt install luajit -y && sudo apt install fzf -y
+sudo apt install imagemagick -y && sudo apt install libmagickwand-dev -y && sudo apt install liblua5.1-0-dev -y && sudo apt install luajit -y && sudo apt install fzf -y && sudo apt install apt
 echo "installed packages" 
 
 echo "installing tmp plugin manager for tmux"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # install plugins from tmux "CTRL space I"
+echo "use <ctr> <space> I to update the plugins"
+
+echo "installing zoxide"
+sudo apt install zoxide
+
 echo "coping tmux conf"
 mkdir -p ~/.config/tmux/ && cp tmux.conf ~/.config/tmux/
 
@@ -18,5 +23,7 @@ mkdir -p ~/.config/kitty/ && cp kitty.conf ~/.config/
 echo "coping nvim conf files"
 cp -r nvim ~/.config/
 
+echo "coping zsh conf"
+zshrc > ~/.zshrc
 
 
